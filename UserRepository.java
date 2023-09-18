08b34f48f01f006f810461c7c294c0bcb0c24bd8
kien5217 <130715130+kien5217@users.noreply.github.com>
2023-09-18 16:52:46 +0700

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.kiendemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boot.kiendemo.User.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    
-}
